2a56a3014e36e76f800c1eb0a677472b9c7850be
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-10-03 14:56:15 -0300

    update obj model

--- clases/S3/ejercicioSistemaSolar/js/ObjectModel.js
+++ clases/S3/ejercicioSistemaSolar/js/ObjectModel.js
@@ -1,20 +1,18 @@
 class ObjectModel {
   name = "Objeto3D";
   object = null;
-  pos = [0, 0, 0];
-  rot = [0, [0, 0, 0]];
-  scale = [1, 1, 1];
   transform = mat4.create();
   children = [];
 
   constructor(attributes, transformations, children) {
     if (attributes?.[0]) this.name = attributes?.[0];
     if (attributes?.[1]) this.object = attributes?.[1];
-    transformations?.[0] && transformations.reverse().forEach(({ pos, rot, scale }) => {
-      pos && mat4.translate(this.transform, this.transform, pos);
-      rot && mat4.rotate(this.transform, this.transform, rot[0], rot[1]);
-      scale && mat4.scale(this.transform, this.transform, scale);
-    });
+    transformations?.[0] &&
+      transformations.reverse().forEach(({ pos, rot, scale }) => {
+        pos && mat4.translate(this.transform, this.transform, pos);
+        rot && mat4.rotate(this.transform, this.transform, rot[0], rot[1]);
+        scale && mat4.scale(this.transform, this.transform, scale);
+      });
     if (children) this.children = children;
   }
 
@@ -27,7 +25,6 @@ class ObjectModel {
     this.children.forEach((child) => child.draw(transform));
   }
 }
-
 
 class Transform {
   static translate = (pos) => ({ pos });
