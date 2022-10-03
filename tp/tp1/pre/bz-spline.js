5da12c7b2af7fda3b1181694cd8daf1f7afcd610
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-10-03 01:02:07 -0300

    bezier-spline

--- tp/tp1/pre/bz-spline.js
+++ tp/tp1/pre/bz-spline.js
@@ -14,86 +14,150 @@ const dB3 = (u) => 3 * u * u;
 
 // Config
 const debug = true;
+const convexity = 1; // 1: Convex, -1: Concave
 const n_delta = 0.01;
 
-const segmentPoint = (u, controlPoints, complete = true) => {
-  const [p0, p1, p2, p3] = controlPoints;
-
-  // Point
-  const p = [
-    B0(u) * p0[0] + B1(u) * p1[0] + B2(u) * p2[0] + B3(u) * p3[0],
-    B0(u) * p0[1] + B1(u) * p1[1] + B2(u) * p2[1] + B3(u) * p3[1],
-    B0(u) * p0[2] + B1(u) * p1[2] + B2(u) * p2[2] + B3(u) * p3[2],
-  ];
-
-  if (!complete) return p;
-
-  // Tangent
-
-  const dp = [
-    dB0(u) * p0[0] + dB1(u) * p1[0] + dB2(u) * p2[0] + dB3(u) * p3[0],
-    dB0(u) * p0[1] + dB1(u) * p1[1] + dB2(u) * p2[1] + dB3(u) * p3[1],
-    dB0(u) * p0[2] + dB1(u) * p1[2] + dB2(u) * p2[2] + dB3(u) * p3[2],
-  ];
-
-  const mod_dp = Math.sqrt(dp[0] * dp[0] + dp[1] * dp[1] + dp[2] * dp[2]);
-
-  const tangent = [dp[0] / mod_dp, dp[1] / mod_dp, dp[2] / mod_dp];
-
-  // Normal
-
-  let dv;
-  if (u < 0.5) {
-    const next_p = segmentPoint(u + n_delta, controlPoints, false);
-    dv = [next_p[0] - p[0], next_p[1] - p[1], next_p[2] - p[2]];
-  } else {
-    const prev_p = segmentPoint(u - n_delta, controlPoints, false);
-    dv = [p[0] - prev_p[0], p[1] - prev_p[1], p[2] - prev_p[2]];
+class Segment {
+  constructor(controlPoints) {
+    this.controlPoints = controlPoints;
   }
 
-  const bi_normal = vec.normalize(newVec(), vec.cross(newVec, tangent, dv));
-  const normal = vec.normalize(newVec(), vec.cross(newVec, bi_normal, tangent));
-  // FIXME: if tg and dv are parallel, normal is undefined
+  length(delta = 0.1) {
+    if (this._len) return this._len;
 
-  if (
-    (debug && vec.dist(newVec(), bi_normal) < 0.1) ||
-    vec.dist(newVec(), normal) < 0.1
-  )
-    console.warn("NORMAL ERROR");
+    let length = 0;
+    for (let u = 0; u < 1 - delta; u += delta) {
+      const p = this.point(u, false);
+      const next_p = this.point(u + delta, false);
+      length += vec.dist(p, next_p);
+    }
 
-  return { p, t: tangent, n: normal, b: bi_normal };
-};
+    this._len = length;
+    return length;
+  }
 
-const drawSegment = (
-  ctx,
-  controlPoints,
-  showControlQuad = false,
-  delta = 0.01,
-  lineWidth = 2
-) => {
-  const [p0, p1, p2, p3] = controlPoints;
-  ctx.lineWidth = lineWidth;
+  point(u, complete = true) {
+    const [p0, p1, p2, p3] = this.controlPoints;
 
-  if (showControlQuad) {
+    // Point
+    const p = [
+      B0(u) * p0[0] + B1(u) * p1[0] + B2(u) * p2[0] + B3(u) * p3[0],
+      B0(u) * p0[1] + B1(u) * p1[1] + B2(u) * p2[1] + B3(u) * p3[1],
+      B0(u) * p0[2] + B1(u) * p1[2] + B2(u) * p2[2] + B3(u) * p3[2],
+    ];
+
+    if (!complete) return p;
+
+    // Tangent
+
+    const dp = [
+      dB0(u) * p0[0] + dB1(u) * p1[0] + dB2(u) * p2[0] + dB3(u) * p3[0],
+      dB0(u) * p0[1] + dB1(u) * p1[1] + dB2(u) * p2[1] + dB3(u) * p3[1],
+      dB0(u) * p0[2] + dB1(u) * p1[2] + dB2(u) * p2[2] + dB3(u) * p3[2],
+    ];
+
+    const mod_dp = Math.sqrt(dp[0] * dp[0] + dp[1] * dp[1] + dp[2] * dp[2]);
+
+    const tangent = [dp[0] / mod_dp, dp[1] / mod_dp, dp[2] / mod_dp];
+
+    // Normal
+    /// FIXME: if tg and dv are parallel, normal is undefined
+    /// FIXME: convexity may vary along the curve
+
+    const next_p = this.point(u + n_delta, false);
+    const dv = [0, 1, 2].map((n) => convexity * (p[n] - next_p[n]));
+
+    const bi_normal = vec.normalize(newVec(), vec.cross(newVec, tangent, dv));
+    const normal = vec.normalize(
+      newVec(),
+      vec.cross(newVec, bi_normal, tangent)
+    );
+
+    if (
+      (debug && vec.dist(newVec(), bi_normal) < 0.1) ||
+      vec.dist(newVec(), normal) < 0.1
+    )
+      console.warn("NORMAL ERROR");
+
+    return { p, t: tangent, n: normal, b: bi_normal };
+  }
+
+  draw(ctx, showControlQuad = false, delta = 0.01, lineWidth = 2) {
+    const [p0, p1, p2, p3] = this.controlPoints;
+    ctx.lineWidth = lineWidth;
+
+    if (showControlQuad) {
+      ctx.beginPath();
+      ctx.strokeStyle = "#999";
+
+      ctx.moveTo(p0[0], p0[1]);
+      ctx.lineTo(p1[0], p1[1]);
+      ctx.lineTo(p2[0], p2[1]);
+      ctx.lineTo(p3[0], p3[1]);
+
+      ctx.stroke();
+    }
+
     ctx.beginPath();
-    ctx.strokeStyle = "#999";
-
+    ctx.strokeStyle = "#000";
     ctx.moveTo(p0[0], p0[1]);
-    ctx.lineTo(p1[0], p1[1]);
-    ctx.lineTo(p2[0], p2[1]);
-    ctx.lineTo(p3[0], p3[1]);
+
+    for (let u = 0; u <= 1.001; u = u + delta) {
+      const p = this.point(u, false);
+      ctx.lineTo(p[0], p[1]);
+    }
 
     ctx.stroke();
   }
+}
 
-  ctx.beginPath();
-  ctx.strokeStyle = "#000";
-  ctx.moveTo(p0[0], p0[1]);
+class Spline {
+  constructor(controlPoints, delta = 0.01) {
+    if (controlPoints.length < 4) throw new Error("Not enough control points");
+    if (controlPoints.length % 3 !== 1)
+      throw new Error("Invalid number of control points");
 
-  for (let u = 0; u <= 1.001; u = u + delta) {
-    const p = segmentPoint(u, controlPoints, false);
-    ctx.lineTo(p[0], p[1]);
+    this.controlPoints = controlPoints;
+    this.delta = delta;
+    this.segNum = (controlPoints.length - 1) / 3;
+
+    let lengths = [];
+    let totalLength = 0;
+
+    const segment = (i) => new Segment(controlPoints.slice(i * 3, i * 3 + 4));
+
+    for (let s = 0; s < this.segNum; s++) {
+      const segLength = segment(s).length();
+      lengths.push(segLength);
+      totalLength += segLength;
+    }
+
+    this.lengths = lengths.map((l) => l / totalLength);
   }
 
-  ctx.stroke();
-};
+  segment(i) {
+    return new Segment(this.controlPoints.slice(i * 3, i * 3 + 4));
+  }
+
+  mapU(u) {
+    let s = 0;
+    // TODO: binary search
+    while (u > this.lengths[s]) {
+      u -= this.lengths[s];
+      s++;
+    }
+
+    return [s, u / this.lengths[s]];
+  }
+
+  point(u) {
+    const [s, su] = this.mapU(u);
+    return this.segment(s).point(su);
+  }
+
+  draw(ctx, showControlQuad = false) {
+    for (let s = 0; s < this.segNum; s++) {
+      this.segment(s).draw(ctx, showControlQuad);
+    }
+  }
+}
